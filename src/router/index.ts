00afb97fe35e3a4999c41ceb2a4d9633ed36dc93
Julian Schmidt <julian.schmidt@nexusgroup.com>
2022-01-25 23:35:22 +0100

    feat: create downloads, datenschutz, impressum

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,40 +1,47 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/music',
-    name: 'Music',
-    component: () => import('../views/Music.vue')
-  },
-  {
-    path: '/band',
-    name: 'Band',
-    component: () => import('../views/Band.vue')
-  },
-  {
-    path: '/shows',
-    name: 'Shows',
-    component: () => import('../views/Shows.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/music',
+        name: 'Music',
+        component: () => import('../views/Music.vue')
+    },
+    {
+        path: '/band',
+        name: 'Band',
+        component: () => import('../views/Band.vue')
+    },
+    {
+        path: '/shows',
+        name: 'Shows',
+        component: () => import('../views/Shows.vue')
+    },
+    {
+        path: '/downloads',
+        name: 'Downloads',
+        component: () => import('../views/Downloads.vue')
+    },
+    {
+        path: '/datenschutzerklaerung',
+        name: 'Datenschutz',
+        component: () => import('../views/Datenschutz.vue')
+    },
+    {
+        path: '/impressum',
+        name: 'Impressum',
+        component: () => import('../views/Impressum.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
---
+++ src/views/Datenschutz.vue
@@ -0,0 +1,242 @@
+<template>
+  <div id="main" class="alt">
+    <section id="one">
+      <div class="inner">
+        <header class="major">
+          <h1> Datenschutzerklärung </h1>
+        </header>
+
+        <h2>1. Datenschutz auf einen Blick</h2>
+        <h3>Allgemeine Hinweise</h3>
+        <p>Die folgenden Hinweise geben einen einfachen &uuml;berblick dar&uuml;ber, was mit Ihren personenbezogenen
+          Daten passiert, wenn Sie unsere Website besuchen. Personenbezogene Daten sind alle Daten, mit denen Sie
+          persönlich identifiziert werden können. Ausf&uuml;hrliche Informationen zum Thema Datenschutz entnehmen
+          Sie unserer unter diesem Text aufgef&uuml;hrten Datenschutzerklärung.</p>
+
+        <h3>Datenerfassung auf unserer Website</h3>
+        <p><strong>Wer ist verantwortlich f&uuml;r die Datenerfassung auf dieser Website?</strong><br/>
+          Die Datenverarbeitung auf dieser Website erfolgt durch den Websitebetreiber (<a href="https://www.ionos.de/">IONOS</a>).
+          Dessen Kontaktdaten können Sie dem Impressum dieser
+          Website entnehmen.</p>
+
+        <p><strong>Wie erfassen wir Ihre Daten?</strong><br/>
+          Ihre Daten werden zum einen dadurch erhoben, dass Sie uns diese mitteilen. Hierbei kann es sich z.B. um
+          Daten handeln, die Sie in ein Kontaktformular eingeben.
+          Andere Daten werden automatisch beim Besuch der Website durch unsere IT-Systeme erfasst. Das sind vor
+          allem technische Daten (z.B. Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs). Die
+          Erfassung dieser Daten erfolgt automatisch, sobald Sie unsere Website betreten.</p>
+
+        <p><strong>Wof&uuml;r nutzen wir Ihre Daten?</strong><br/>
+          Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung der Website zu gewährleisten. Andere
+          Daten können zur Analyse Ihres Nutzerverhaltens verwendet werden. Außerdem werden Ihre Daten im Falle
+          eines Auftrags zur Verarbeitung und Abwicklung dessen genutzt. </p>
+
+        <p><strong>Welche Rechte haben Sie bez&uuml;glich Ihrer Daten?</strong><br/>
+          Sie haben jederzeit das Recht, unentgeltlich Auskunft &uuml;ber Herkunft, Empfänger und Zweck Ihrer
+          gespeicherten personenbezogenen Daten zu erhalten. Sie haben außerdem ein Recht, die Berichtigung,
+          Sperrung oder Löschung dieser Daten zu verlangen. Hierzu sowie zu weiteren Fragen zum Thema Datenschutz
+          können Sie sich jederzeit unter der im Impressum angegebenen Adresse an uns wenden. Des Weiteren steht
+          Ihnen ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu. Außerdem haben Sie das Recht, unter
+          bestimmten Umständen die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen.
+          Details hierzu entnehmen Sie der Datenschutzerklärung unter
+          „Recht auf Einschränkung der Verarbeitung“.</p>
+
+
+        <h2>2. Allgemeine Hinweise und Pflichtinformationen</h2>
+
+        <h3>Datenschutz</h3>
+        <p>Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre
+          personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie
+          dieser Datenschutzerklärung.
+          Wenn Sie diese Website benutzen, werden verschiedene personenbezogene Daten erhoben.
+          Personenbezogene Daten sind Daten, mit denen Sie persönlich identifiziert werden können. Die vorliegende
+          Datenschutzerklärung erläutert, welche Daten wir erheben und wof&uuml;r wir sie nutzen. Sie erläutert
+          auch, wie und zu welchem Zweck das geschieht.
+          Wir weisen darauf hin, dass die Daten&uuml;bertragung im Internet (z.B. bei der Kommunikation per
+          E-Mail) Sicherheitsl&uuml;cken aufweisen kann. Ein l&uuml;ckenloser Schutz der Daten vor dem Zugriff
+          durch Dritte ist nicht möglich.</p>
+
+        <h3>Hinweis zur verantwortlichen Stelle</h3>
+        <p>Die verantwortliche Stelle f&uuml;r die Datenverarbeitung auf dieser Website ist:<br/>
+
+          Julian Schmidt<br/>
+          Ludwig-Marum-Str. 38<br/>
+          76185 Karlsruhe<br/>
+          Telefon: 01781429505<br/>
+          E-Mail: <a href="mailto:contact(at)clublive.band">contact(at)clublive.band</a><br/>
+
+          Verantwortliche Stelle ist die nat&uuml;rliche oder juristische Person, die allein oder gemeinsam mit
+          anderen &uuml;ber die Zwecke und Mittel der Verarbeitung von personenbezogenen Daten (z.B. Namen,
+          E-Mail-Adressen o. Ä.) entscheidet.</p>
+
+        <h3>Widerruf Ihrer Einwilligung zur Datenverarbeitung</h3>
+        <p>Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdr&uuml;cklichen Einwilligung möglich. Sie können
+          eine bereits erteilte Einwilligung jederzeit widerrufen. Dazu reicht eine formlose Mitteilung per E-Mail
+          an uns. Die Rechtmäßigkeit der bis zum Widerruf erfolgten Datenverarbeitung bleibt vom Widerruf unber&uuml;hrt.</p>
+
+        <h3>Widerspruchsrecht gegen die Datenerhebung in besonderen Fällen sowie gegen
+          Direktwerbung (Art. 21 DSGVO)</h3>
+        <p><strong>Wenn die Datenverarbeitung auf Grundlage von Art. 6 Abs. 1 lit. e oder f DSGVO erfolgt, haben Sie
+          jederzeit das Recht, aus Gr&uuml;nden, die sich aus Ihrer besonderen Situation ergeben, gegen die
+          Verarbeitung Ihrer personenbezogenen Daten Widerspruch einzulegen; dies gilt auch f&uuml;r ein auf diese
+          Bestimmungen gest&uuml;tztes Profiling. Die jeweilige Rechtsgrundlage, auf denen eine Verarbeitung
+          beruht, entnehmen Sie dieser Datenschutzerklärung. Wenn Sie Widerspruch einlegen, werden wir Ihre
+          betroffenen personenbezogenen Daten nicht mehr verarbeiten, es sei denn, wir können zwingende schutzw&uuml;rdige
+          Gr&uuml;nde f&uuml;r die Verarbeitung nachweisen, die Ihre Interessen, Rechte und Freiheiten &uuml;berwiegen
+          oder die Verarbeitung dient der Geltendmachung, Aus&uuml;bung oder Verteidigung von Rechtsanspr&uuml;chen
+          (Widerspruch nach Art. 21 Abs. 1 DSGVO).
+          Werden Ihre personenbezogenen Daten verarbeitet, um Direktwerbung zu betreiben, so haben Sie das Recht,
+          jederzeit Widerspruch gegen die Verarbeitung Sie betreffender personenbezogener Daten zum Zwecke
+          derartiger Werbung einzulegen; dies gilt auch f&uuml;r das Profiling, soweit es mit solcher
+          Direktwerbung in Verbindung steht. Wenn Sie widersprechen, werden Ihre personenbezogenen Daten
+          anschließend nicht mehr zum Zwecke der Direktwerbung verwendet (Widerspruch nach Art. 21 Abs. 2
+          DSGVO).</strong></p>
+
+        <h3>Beschwerderecht bei der zuständigen Aufsichtsbehörde</h3>
+        <p>Im Falle von Verstößen gegen die DSGVO steht den Betroffenen ein Beschwerderecht bei einer
+          Aufsichtsbehörde, insbesondere in dem Mitgliedstaat ihres gewöhnlichen Aufenthalts, ihres Arbeitsplatzes
+          oder des Orts des mutmaßlichen Verstoßes zu. Das Beschwerderecht besteht unbeschadet anderweitiger
+          verwaltungsrechtlicher oder gerichtlicher Rechtsbehelfe.</p>
+
+        <h3>Recht auf Daten&uuml;bertragbarkeit</h3>
+        <p>Sie haben das Recht, Daten, die wir auf Grundlage Ihrer Einwilligung oder in Erf&uuml;llung eines
+          Vertrags
+          automatisiert verarbeiten, an sich oder an einen Dritten in einem gängigen, maschinenlesbaren Format
+          aushändigen zu lassen. Sofern Sie die direkte &uuml;bertragung der Daten an einen anderen
+          Verantwortlichen verlangen, erfolgt dies nur, soweit es technisch machbar ist.</p>
+
+        <h3>SSL- bzw. TLS-Verschl&uuml;sselung</h3>
+        <p>Diese Seite nutzt aus Sicherheitsgr&uuml;nden und zum Schutz der &uuml;bertragung vertraulicher Inhalte,
+          wie zum Beispiel Bestellungen oder Anfragen, die Sie an uns als Seitenbetreiber senden, eine SSL-bzw.
+          TLSVerschl&uuml;sselung. Eine verschl&uuml;sselte Verbindung erkennen Sie daran, dass die Adresszeile
+          des Browsers von “http://” auf “https://” wechselt und an dem Schloss-Symbol in Ihrer Browserzeile.
+          Wenn die SSL- bzw. TLS-Verschl&uuml;sselung aktiviert ist, können die Daten, die Sie an uns &uuml;bermitteln,
+          nicht von Dritten mitgelesen werden.</p>
+
+        <h3>Auskunft, Sperrung, Löschung und Berichtigung</h3>
+        <p>Sie haben im Rahmen der geltenden gesetzlichen Bestimmungen jederzeit das Recht auf unentgeltliche
+          Auskunft &uuml;ber Ihre gespeicherten personenbezogenen Daten, deren Herkunft und Empfänger und den
+          Zweck der Datenverarbeitung und ggf. ein Recht auf Berichtigung, Sperrung oder Löschung dieser Daten.
+          Hierzu sowie zu weiteren Fragen zum Thema personenbezogene Daten können Sie sich jederzeit unter der im
+          Impressum angegebenen Adresse an uns wenden.</p>
+
+        <h3>Recht auf Einschränkung der Verarbeitung</h3>
+        <p><strong>Sie haben das Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu
+          verlangen. Hierzu können Sie sich jederzeit unter der im Impressum angegebenen Adresse an uns wenden.
+          Das Recht auf Einschränkung der Verarbeitung besteht in folgenden Fällen:</strong>
+          Wenn Sie die Richtigkeit Ihrer bei uns gespeicherten personenbezogenen Daten bestreiten, benötigen wir
+          in der Regel Zeit, um dies zu &uuml;berpr&uuml;fen. F&uuml;r die Dauer der Pr&uuml;fung haben Sie das
+          Recht, die
+          Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Wenn die Verarbeitung Ihrer
+          personenbezogenen Daten unrechtmäßig geschah / geschieht, können Sie
+          statt der Löschung die Einschränkung der Datenverarbeitung verlangen. Wenn wir Ihre personenbezogenen
+          Daten nicht mehr benötigen, Sie sie jedoch zur Aus&uuml;bung, Verteidigung oder Geltendmachung von
+          Rechtsanspr&uuml;chen benötigen, haben Sie das Recht, statt der Löschung die Einschränkung der
+          Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Wenn Sie einen Widerspruch nach Art. 21 Abs. 1
+          DSGVO eingelegt haben, muss eine Abwägung zwischen Ihren und unseren Interessen vorgenommen werden.
+          Solange noch nicht feststeht, wessen Interessen
+          &uuml;berwiegen, haben Sie das Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten
+          zu verlangen. Wenn Sie die Verarbeitung Ihrer personenbezogenen Daten eingeschränkt haben, d&uuml;rfen
+          diese Daten – von ihrer Speicherung abgesehen – nur mit Ihrer Einwilligung oder zur Geltendmachung, Aus&uuml;bung
+          oder Verteidigung von Rechtsanspr&uuml;chen oder zum Schutz der Rechte einer anderen nat&uuml;rlichen
+          oder juristischen Person oder aus Gr&uuml;nden eines wichtigen öffentlichen Interesses der Europäischen
+          Union oder eines Mitgliedstaats verarbeitet werden.</p>
+
+        <h2 id="content">3. Datenerfassung auf unserer Website</h2>
+        <h3>Server-Log-Dateien</h3>
+        <p>Der Provider der Seiten (1&amp;1) erhebt und speichert automatisch Informationen in so genannten
+          Server-Log-Dateien, die Ihr Browser automatisch an uns &uuml;bermittelt. Dies sind:</p>
+        <ul>
+          <li> Referrer (zuvor besuchte Webseite)</li>
+          <li> Angeforderte Webseite oder Datei</li>
+          <li> Browsertyp und Browserversion</li>
+          <li> Verwendetes Betriebssystem</li>
+          <li> Verwendeter Gerätetyp</li>
+          <li> Uhrzeit des Zugriffs</li>
+          <li> IP-Adresse in anonymisierter Form (wird nur zur Feststellung des Orts des Zugriffs verwendet)</li>
+        </ul>
+
+        <p>Eine Zusammenf&uuml;hrung dieser Daten mit anderen Datenquellen wird nicht vorgenommen. Die Erfassung
+          dieser Daten erfolgt auf Grundlage von Art. 6 Abs. 1 lit. f DSGVO. Der Websitebetreiber hat
+          ein berechtigtes Interesse an der technisch fehlerfreien Darstellung und der Optimierung seiner Website
+          – hierzu m&uuml;ssen die Server-Log-Files erfasst werden. Die Speicherdauer der anonymisierten Daten
+          beträgt 8 Wochen.</p>
+
+        <h3>Kontaktformular</h3>
+        <p>Wenn Sie uns per Kontaktformular Anfragen zukommen lassen, werden Ihre Angaben aus dem Anfrageformular
+          inklusive der von Ihnen dort angegebenen Kontaktdaten zwecks Bearbeitung der Anfrage und f&uuml;r den
+          Fall von Anschlussfragen bei uns gespeichert. Diese Daten geben wir nicht ohne Ihre Einwilligung weiter.
+          Die Verarbeitung der in das Kontaktformular eingegebenen Daten erfolgt somit ausschließlich auf
+          Grundlage Ihrer Einwilligung (Art. 6 Abs. 1 lit. a DSGVO). Sie können diese Einwilligung jederzeit
+          widerrufen. Dazu reicht eine formlose Mitteilung per E-Mail an uns. Die Rechtmäßigkeit der bis zum
+          Widerruf erfolgten Datenverarbeitungsvorgänge bleibt vom Widerruf unber&uuml;hrt. Die von Ihnen im
+          Kontaktformular eingegebenen Daten verbleiben bei uns, bis Sie uns zur Löschung auffordern, Ihre
+          Einwilligung zur Speicherung widerrufen oder der Zweck f&uuml;r die Datenspeicherung entfällt (z.B. nach
+          abgeschlossener Bearbeitung Ihrer Anfrage). Zwingende gesetzliche Bestimmungen – insbesondere
+          Aufbewahrungsfristen – bleiben unber&uuml;hrt.</p>
+
+        <h3>Anfrage per E-Mail oder Telefon</h3>
+        <p>Wenn Sie uns per E-Mail oder Telefon kontaktieren, wird Ihre Anfrage inklusive aller daraus
+          hervorgehenden personenbezogenen Daten (Name, Anfrage) zum Zwecke der Bearbeitung Ihres Anliegens bei
+          uns gespeichert und verarbeitet. Diese Daten geben wir nicht ohne Ihre Einwilligung weiter. Die
+          Verarbeitung dieser Daten erfolgt auf Grundlage von Art. 6 Abs. 1 lit. b DSGVO, sofern Ihre Anfrage mit
+          der Erf&uuml;llung eines Vertrags zusammenhängt oder zur Durchf&uuml;hrung vorvertraglicher Maßnahmen
+          erforderlich ist. In allen &uuml;brigen Fällen beruht die Verarbeitung auf Ihrer Einwilligung (Art. 6
+          Abs. 1 lit. a DSGVO) und / oder auf unseren berechtigten Interessen (Art. 6 Abs. 1 lit. f DSGVO), da wir
+          ein berechtigtes Interesse an der effektiven Bearbeitung der an uns gerichteten Anfragen haben. Die von
+          Ihnen an uns per Kontaktanfragen &uuml;bersandten Daten verbleiben bei uns, bis Sie uns zur Löschung
+          auffordern, Ihre Einwilligung zur Speicherung widerrufen oder der Zweck f&uuml;r die Datenspeicherung
+          entfällt (z. B. nach abgeschlossener Bearbeitung Ihres Anliegens). Zwingende gesetzliche Bestimmungen –
+          insbesondere gesetzliche Aufbewahrungsfristen – bleiben unber&uuml;hrt. </p>
+
+        <h2 id="content">4. Plugins und Tools</h2>
+        <h3>YouTube mit erweitertem Datenschutz</h3>
+        <p>Unsere Website nutzt Plugins der Website YouTube. Betreiber der Seiten ist die YouTube, LLC, 901 Cherry
+          Ave., San Bruno, CA 94066, USA. Wir nutzen YouTube im erweiterten Datenschutzmodus. Dieser Modus bewirkt
+          laut YouTube, dass YouTube keine Informationen &uuml;ber die Besucher auf dieser Website speichert,
+          bevor diese sich das Video ansehen. Die Weitergabe von Daten an YouTube-Partner wird durch den
+          erweiterten Datenschutzmodus hingegen nicht zwingend ausgeschlossen. So stellt YouTube – unabhängig
+          davon, ob Sie sich ein Video ansehen – eine Verbindung zum Google DoubleClick-Netzwerk her. Sobald Sie
+          ein YouTube-Video auf unserer Website starten, wird eine Verbindung zu den Servern von YouTube
+          hergestellt. Dabei wird dem YouTube-Server mitgeteilt, welche unserer Seiten Sie besucht haben. Wenn Sie
+          in Ihrem YouTube-Account eingeloggt sind, ermöglichen Sie YouTube, Ihr Surfverhalten direkt Ihrem
+          persönlichen Profil zuzuordnen. Dies können Sie verhindern, indem Sie sich aus Ihrem YouTube- Account
+          ausloggen. Des Weiteren kann YouTube nach Starten eines Videos verschiedene Cookies auf Ihrem Endgerät
+          speichern. Mit Hilfe dieser Cookies kann YouTube Informationen &uuml;ber Besucher unserer Website
+          erhalten. Diese Informationen werden u. a. verwendet, um Videostatistiken zu erfassen, die
+          Anwenderfreundlichkeit zu verbessern und Betrugsversuchen vorzubeugen. Die Cookies verbleiben auf Ihrem
+          Endgerät, bis Sie sie löschen.
+          Gegebenenfalls können nach dem Start eines YouTube-Videos weitere Datenverarbeitungsvorgänge
+          ausgelöst werden, auf die wir keinen Einfluss haben. Die Nutzung von YouTube erfolgt im Interesse einer
+          ansprechenden Darstellung unserer Online-Angebote. Dies stellt ein berechtigtes Interesse im Sinne von
+          Art. 6 Abs. 1 lit. f DSGVO dar. Weitere Informationen &uuml;ber Datenschutz bei YouTube finden Sie in
+          deren <a href="https://policies.google.com/privacy?hl=de">Datenschutzerklärung</a>. </p>
+
+        <h3>Google Web Fonts</h3>
+        <p>Diese Seite nutzt zur einheitlichen Darstellung von Schriftarten so genannte Web Fonts, die von Google
+          bereitgestellt werden. Die Google Fonts sind lokal installiert. Eine Verbindung zu Servern von Google
+          findet dabei nicht statt.</p>
+
+        <h3>Google reCAPTCHA</h3>
+        <p>
+          Wir verwenden Google reCAPTCHA der Firma Google Inc. (1600 Amphitheatre Parkway Mountain View, CA 94043,
+          USA) um Spam zu verhindern. Rechtsgrundlage für die Verwendung ist Artikel 6 (1) f (Rechtmäßigkeit der
+          Verarbeitung), denn es besteht ein berechtigtes Interesse diese Webseite vor Bots und Spam zu schützen.
+          reCAPTCHA ist ein kostenloser Dienst, der Webseiten vor Spam und Missbrauch schützt. Es nutzt
+          fortschrittliche Risikoanalysetechniken, um Menschen und Bots auseinander zu halten. Mit der neuen API
+          wird eine signifikante Anzahl Ihrer gültigen menschlichen Benutzer die reCAPTCHA-Herausforderung
+          bestehen, ohne ein CAPTCHA lösen zu müssen. Wir nutzen reCAPTCHA für die Absicherung von
+          Formularen. Durch die Nutzung von reCAPTCHA werden Daten an Google übertragen die Google nutzt um
+          festzustellen ob der Besucher ein Mensch oder ein (Spam)bot ist. Welche Daten von Google erfasst werden
+          und wofür diese Daten verwendet werden, können Sie auf
+          <a href="https://policies.google.com/privacy?hl=de-DE">https://policies.google.com/privacy?hl=de-DE</a>
+          nachlesen. Die Nutzungsbedingungen für Dienste und Produkte von Google können Sie unter
+          <a herf="https://policies.google.com/terms?hl=de-DE">https://policies.google.com/terms?hl=de-DE</a>
+          nachlesen.
+        </p>
+
+        <p><strong>Quelle: <a href="https://www.e-recht24.de"> e-recht24.de </a></strong></p>
+      </div>
+    </section>
+  </div>
+</template>
