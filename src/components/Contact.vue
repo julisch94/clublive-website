b9cd614a5a5ed6cb53d526d575fcbaa37c406cd8
Julian Schmidt <julian.schmidt@cobalt.io>
2022-04-04 17:49:44 +0200

    fix: send form data

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,48 +3,54 @@
     <div class="inner">
       <section>
         <h3>Kontaktformular / Ihre Anfrage</h3>
-        <form @submit.prevent="submitForm()" @reset.prevent="reset()" data-netlify="true" data-netlify-recaptcha="true">
+        <form
+          @submit.prevent="submitForm"
+          @reset.prevent="reset()"
+          data-netlify="true"
+          data-netlify-recaptcha="true"
+        >
           <input type="hidden" name="form-name" value="contact" />
           <div class="field half first">
             <label for="name">Name</label>
-            <input
-                type="text"
-                name="name"
-                id="name"
-                v-model="this.name"
-                required>
+            <input type="text" name="name" id="name" v-model="name" required />
           </div>
           <div class="field half">
             <label for="email">E-Mail</label>
             <input
-                type="text"
-                name="email"
-                id="email"
-                v-model="this.email"
-                pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,}$"
-                required>
+              type="text"
+              name="email"
+              id="email"
+              v-model="email"
+              pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,}$"
+              required
+            />
           </div>
           <div class="field">
             <label for="message">Nachricht</label>
             <textarea
-                name="message"
-                id="message"
-                v-model="this.message"
-                rows="6"
-                @keyup.ctrl.enter="submitForm()">
+              name="message"
+              id="message"
+              v-model="message"
+              rows="6"
+              @keyup.ctrl.enter="submitForm()"
+            >
             </textarea>
           </div>
           <div data-netlify-recaptcha="true"></div>
           <ul class="actions">
-            <li><input type="submit" value="Abschicken" class="special"></li>
-            <li><input type="reset" value="Löschen"></li>
+            <li><input type="submit" value="Abschicken" class="special" /></li>
+            <li><input type="reset" value="Löschen" /></li>
           </ul>
         </form>
         <div class="result">
-          <p v-if="this.success">Vielen Dank! Die Nachricht wurde übermittelt. Wir melden uns baldmöglichst
-            zurück.</p>
-          <p v-if="this.failure">Oh. Da ist etwas schief gegangen. Kannst du uns bitte stattdessen eine E-Mail an
-            contact@clublive.band schicken?</p>
+          <p v-if="success">
+            Vielen Dank! Die Nachricht wurde übermittelt. Wir melden uns
+            baldmöglichst zurück.
+          </p>
+          <p v-if="failure">
+            Oh. Da ist etwas schief gegangen. Kannst du uns bitte stattdessen
+            eine E-Mail an contact@clublive.band schicken?
+          </p>
         </div>
       </section>
       <section class="split">
@@ -60,7 +66,7 @@
           <div class="contact-method">
             <span class="icon alt fa-home"></span>
             <h3>Adresse</h3>
-            <span>76131 Karlsruhe<br>Baden-Württemberg<br>Deutschland</span>
+            <span>76131 Karlsruhe<br />Baden-Württemberg<br />Deutschland</span>
           </div>
         </section>
       </section>
@@ -69,18 +75,21 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Contact",
   data() {
     return {
-      name: '',
-      email: '',
-      message: '',
+      name: "bla",
+      email: "",
+      message: "",
       success: false,
-      failure: false
-    }
+      failure: false,
+    };
+  },
+  mounted() {
+    console.log(this.name);
   },
   methods: {
     submitForm() {
@@ -88,42 +97,42 @@ export default defineComponent({
         "form-name": "contact",
         name: this.name,
         email: this.email,
-        message: this.message
+        message: this.message,
       };
+      console.log("Sending form with the following data", body);
 
       fetch("/", {
         method: "POST",
-        headers: {"Content-Type": "application/x-www-form-urlencoded"},
-        body: new URLSearchParams(body).toString()
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(body).toString(),
       })
-          .then((result) => {
-            console.log(result);
-            if (result.ok) {
-              this.success = true;
-              console.log("Form successfully submitted.");
-            } else {
-              this.failure = true;
-              console.error(
-                  "Form submission failed",
-                  result.status,
-                  result.statusText
-              );
-            }
-          })
-          .catch((error) => {
-            alert(error);
-          });
+        .then((result) => {
+          console.log(result);
+          if (result.ok) {
+            this.success = true;
+            console.log("Form successfully submitted.");
+          } else {
+            this.failure = true;
+            console.error(
+              "Form submission failed",
+              result.status,
+              result.statusText
+            );
+          }
+        })
+        .catch((error) => {
+          alert(error);
+        });
     },
     reset() {
-      this.name = '';
-      this.email = '';
-      this.message = '';
+      this.name = "";
+      this.email = "";
+      this.message = "";
       this.success = false;
       this.failure = false;
-    }
-  }
-})
-
+    },
+  },
+});
 </script>
 
 <style>
