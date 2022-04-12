a78ae6755ad7fa3e1522b0f74068af65887aa7ee
Julian Schmidt <julian.schmidt@cobalt.io>
2022-04-12 23:27:29 +0200

    feat: improve footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,75 +3,90 @@
     <div class="inner">
       <ul class="icons">
         <li>
-          <a href="https://facebook.com/ClubLive.Band" class="icon alt fa-facebook">
+          <a
+            href="https://facebook.com/ClubLive.Band"
+            class="icon alt fa-facebook"
+          >
             <span class="label">Facebook</span>
           </a>
         </li>
         <li>
-          <a href="https://instagram.com/clublive.band" class="icon alt fa-instagram">
+          <a
+            href="https://instagram.com/clublive.band"
+            class="icon alt fa-instagram"
+          >
             <span class="label">Instagram</span>
           </a>
         </li>
         <li>
-          <a href="https://www.youtube.com/channel/UCpnhZgIseF5XWs5icvU4nuQ" class="icon alt fa-youtube">
+          <a
+            href="https://www.youtube.com/channel/UCpnhZgIseF5XWs5icvU4nuQ"
+            class="icon alt fa-youtube"
+          >
             <span class="label">YouTube</span>
           </a>
         </li>
       </ul>
       <ul class="copyright">
-        <li>&copy; Bilder von <a href="https://www.instagram.com/romanbischoff.photography/">Roman Bischoff</a> und <a
-            href="https://www.instagram.com/yannick_bodemer_fotografie/">Yannick Bodemer</a></li>
+        <li>
+          Bilder von
+          <a href="https://www.romanbischoff.com/">Roman Bischoff</a>,
+          <a href="https://www.tkammies.com/">Timothée Kammies</a> und
+          <a href="https://ybfotografie.com/">Yannick Bodemer</a>.
+        </li>
       </ul>
       <ul class="copyright">
         <li>&copy; {{ currentYear }} ClubLive</li>
-        <li>Design: <a href="https://html5up.net" target="_blank">HTML5 UP</a></li>
-        <li>Vue.js integration: <a href="https://github.com/julisch94">Julian Schmidt</a></li>
+        <li>
+          Design: <a href="https://html5up.net" target="_blank">HTML5 UP</a>
+        </li>
+        <li>
+          Vue.js integration:
+          <a href="https://julianschmidt.codes/">Julian Schmidt</a>
+        </li>
       </ul>
     </div>
   </footer>
 </template>
 
 <script lang="ts">
-import {Vue} from "vue-class-component";
+import { Vue } from "vue-class-component";
 
 export default class Footer extends Vue {
-
   get currentYear(): number {
     return new Date().getFullYear();
   }
-
 }
 </script>
 
 <style>
-
 #footer .copyright {
-    font-size: 0.8em;
-    list-style: none;
-    padding-left: 0
+  font-size: 0.8em;
+  list-style: none;
+  padding-left: 0;
 }
 
 #footer .copyright li {
-    border-left: solid 1px white;
-    display: inline-block;
-    line-height: 1;
-    margin-left: 1em;
-    padding-left: 1em
+  border-left: solid 1px white;
+  display: inline-block;
+  line-height: 1;
+  margin-left: 1em;
+  padding-left: 1em;
 }
 
 #footer .copyright li:first-child {
-    border-left: 0;
-    margin-left: 0;
-    padding-left: 0
+  border-left: 0;
+  margin-left: 0;
+  padding-left: 0;
 }
 
 @media screen and (max-width: 760px) {
-    #footer .copyright li {
-        display: block;
-        border-left: 0;
-        margin-left: 0;
-        padding-left: 0;
-        line-height: inherit
-    }
+  #footer .copyright li {
+    display: block;
+    border-left: 0;
+    margin-left: 0;
+    padding-left: 0;
+    line-height: inherit;
+  }
 }
 </style>
