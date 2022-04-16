f72813038e548f7ba94769b452cea180ad716921
Julian Schmidt <contact@julianschmidt.codes>
2022-04-16 14:43:45 +0200

    display past shows and reduce show image (#6)
    * feat: show past shows
    
    * feat(shows): resize image to reduce loading time
    
    * feat: show no banner for shows subpage
    
    * feat(shows): resize image
    
    * feat(shows): show banner image again

--- src/views/Shows.vue
+++ src/views/Shows.vue
@@ -10,33 +10,49 @@
   <div class="alt" id="main">
     <section id="one">
       <div class="inner">
-
         <div v-if="hasFutureShows">
-          <Show v-for="show of futureShows" :key="show.date" :show="show"/>
-          <hr/>
+          <Show v-for="show of futureShows" :key="show.date" :show="show" />
+          <hr />
           <p>Weitere Auftritte werden regelmäßig bekannt gegeben.</p>
         </div>
 
         <div v-else>
           <h3 id="noShows">Bald werden wir wieder durchstarten! #corona</h3>
-          <p>Diese Seite wird regelmäßig von uns aktualisiert.</p>
+          <p>Sobald neue Auftritte anstehen, erscheinen sie hier.</p>
         </div>
       </div>
+    </section>
 
+    <section>
+      <div class="inner">
+        <h3>Vergangene Shows</h3>
+        <ul>
+          <PastShow
+            v-for="show in sortedPastShows"
+            :key="show.date"
+            :show="show"
+          />
+          <li>...</li>
+        </ul>
+      </div>
     </section>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
-import Show from "../components/Show.vue";
-import {ShowModel} from "@/model/show.model";
+import { defineComponent } from "vue";
+
 import dayjs from "dayjs";
+
+import Show from "@/components/Show.vue";
+import { ShowModel } from "@/model/show.model";
+import PastShow from "@/components/PastShow.vue";
 
 export default defineComponent({
   name: "Shows",
   components: {
-    Show
+    Show,
+    PastShow,
   },
   data() {
     return {
@@ -46,25 +62,70 @@ export default defineComponent({
           place: "Sommerfest Erasmus Karlsruhe",
           description: "Eintritt frei. Open Air im Forum des KIT Campus.",
           website: "https://karlsruhe.esn-germany.de/",
-          mapsLink: "https://goo.gl/maps/GbzK9vumR5MufVzy7"
+          mapsLink: "https://goo.gl/maps/GbzK9vumR5MufVzy7",
         },
-      ]
-    }
+      ],
+      pastShows: [
+        {
+          date: "2018/03/09",
+          text: "Dorfschänke Karlsruhe",
+        },
+        {
+          date: "2018/07/04",
+          text: "Erasmus Sommerfest, KIT Campus, Karlsruhe",
+        },
+        {
+          date: "2018/10/19",
+          text: "Dorfschänke Karlsruhe",
+        },
+        {
+          date: "2019/04/26",
+          text: "Dorfschänke Karlsruhe",
+        },
+        {
+          date: "2019/05/11",
+          text: "Irish Pub Pforzheim",
+        },
+        {
+          date: "2019/07/18",
+          text: "Erasmus Sommerfest, KIT Campus, Karlsruhe",
+        },
+        {
+          date: "2019/07/19",
+          text: "Private Veranstaltung, Rheinfelden bei Lörrach",
+        },
+        {
+          date: "2019/11/09",
+          text: "Irish Pub Pforzheim",
+        },
+        {
+          date: "2022/04/02",
+          text: "Irish Pub Pforzheim",
+        },
+      ],
+    };
   },
   computed: {
     futureShows(): ShowModel[] {
-      return this.shows.filter(show => !dayjs().isAfter(show.date));
+      return this.shows.filter((show) => !dayjs().isAfter(show.date));
     },
     hasFutureShows(): boolean {
       return this.futureShows.length > 0;
-    }
-  }
-})
-
+    },
+    sortedPastShows(): any[] {
+      const s = JSON.parse(JSON.stringify(this.pastShows));
+      return s.sort((first: any, second: any) => {
+        return dayjs(first.date).isBefore(dayjs(second.date)) ? 1 : -1;
+      });
+    },
+  },
+});
 </script>
 
 <style scoped>
 #banner {
   background-image: url("../assets/images/shows.jpg");
+  background-size: cover;
+  background-position: center;
 }
 </style>
