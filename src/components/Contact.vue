e38c48002a0719ff9a50f7d793f16dbc3bc814f9
Julian Schmidt <contact@julianschmidt.codes>
2022-04-16 19:15:38 +0200

    feat(contact): show loader; add animation (#7)

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,53 +3,78 @@
     <div class="inner">
       <section>
         <h3>Kontaktformular / Ihre Anfrage</h3>
-        <form
-          @submit.prevent="submitForm"
-          @reset.prevent="reset()"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <div class="field half first">
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name" v-model="name" required />
+        <div class="overlay-container">
+          <div class="loading-overlay" v-if="isLoading">
+            <Loader color="#efefef" />
           </div>
-          <div class="field half">
-            <label for="email">E-Mail</label>
-            <input
-              type="text"
-              name="email"
-              id="email"
-              v-model="email"
-              pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,}$"
-              required
-            />
+          <form
+            @submit.prevent="submitForm"
+            @reset.prevent="reset()"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            <div class="field half first">
+              <label for="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                v-model="name"
+                required
+              />
+            </div>
+            <div class="field half">
+              <label for="email">E-Mail</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                v-model="email"
+                pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,}$"
+                required
+              />
+            </div>
+            <div class="field">
+              <label for="message">Nachricht</label>
+              <textarea
+                name="message"
+                id="message"
+                v-model="message"
+                rows="6"
+                @keyup.ctrl.enter="submitForm()"
+              >
+              </textarea>
+            </div>
+            <div data-netlify-recaptcha="true"></div>
+            <ul class="actions">
+              <li>
+                <button type="submit" class="special" :disabled="isLoading">
+                  Abschicken
+                </button>
+              </li>
+              <li>
+                <button type="reset" :disabled="isLoading">Löschen</button>
+              </li>
+            </ul>
+          </form>
+          <div class="result" :class="{ shake: success || failure }">
+            <div v-if="success">
+              <span class="icon alt fa-check"></span>
+              <span
+                >Vielen Dank! Die Nachricht wurde übermittelt. Wir melden uns
+                baldmöglichst zurück.</span
+              >
+            </div>
+            <div v-if="failure">
+              <span class="icon alt fa-exclamation"></span>
+              <span>
+                Oh. Da ist etwas schief gegangen. Kannst du uns bitte
+                stattdessen eine E-Mail an
+                <a href="mailto:contact@clublive.band">contact@clublive.band</a>
+                schicken?
+              </span>
+            </div>
           </div>
-          <div class="field">
-            <label for="message">Nachricht</label>
-            <textarea
-              name="message"
-              id="message"
-              v-model="message"
-              rows="6"
-              @keyup.ctrl.enter="submitForm()"
-            >
-            </textarea>
-          </div>
-          <div data-netlify-recaptcha="true"></div>
-          <ul class="actions">
-            <li><input type="submit" value="Abschicken" class="special" /></li>
-            <li><input type="reset" value="Löschen" /></li>
-          </ul>
-        </form>
-        <div class="result">
-          <p v-if="success">
-            Vielen Dank! Die Nachricht wurde übermittelt. Wir melden uns
-            baldmöglichst zurück.
-          </p>
-          <p v-if="failure">
-            Oh. Da ist etwas schief gegangen. Kannst du uns bitte stattdessen
-            eine E-Mail an contact@clublive.band schicken?
-          </p>
         </div>
       </section>
       <section class="split">
@@ -57,7 +82,7 @@
           <div class="contact-method">
             <span class="icon alt fa-envelope"></span>
             <h3>E-Mail</h3>
-            <span><a href="mailto:contact@clublive.band">contact@clublive.band</a></span>
+            <a href="mailto:contact@clublive.band">contact@clublive.band</a>
             <p>Wir freuen uns über Ihre Anfrage!</p>
           </div>
         </section>
@@ -75,9 +100,13 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Loader from "vue-spinner/src/ScaleLoader.vue";
 
 export default defineComponent({
   name: "Contact",
+  components: {
+    Loader,
+  },
   data() {
     return {
       name: "",
@@ -85,10 +114,8 @@ export default defineComponent({
       message: "",
       success: false,
       failure: false,
+      isLoading: false,
     };
-  },
-  mounted() {
-    console.log(this.name);
   },
   methods: {
     submitForm() {
@@ -98,8 +125,11 @@ export default defineComponent({
         email: this.email,
         message: this.message,
       };
-      console.log("Sending form with the following data", body);
+      console.log("form data", body);
 
+      this.success = false;
+      this.failure = false;
+      this.isLoading = true;
       fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -107,6 +137,7 @@ export default defineComponent({
       })
         .then((result) => {
           console.log(result);
+          this.isLoading = false;
           if (result.ok) {
             this.success = true;
             console.log("Form successfully submitted.");
@@ -120,6 +151,7 @@ export default defineComponent({
           }
         })
         .catch((error) => {
+          this.isLoading = false;
           alert(error);
         });
     },
@@ -137,5 +169,57 @@ export default defineComponent({
 <style>
 div.result {
   margin-top: 1em;
+}
+
+.overlay-container {
+  position: relative;
+}
+
+.loading-overlay {
+  position: absolute;
+  background-color: #00000044;
+  user-select: none;
+  top: -10px;
+  bottom: -10px;
+  left: -10px;
+  right: -10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+}
+
+.result div {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+}
+
+.shake {
+  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  transform: translate3d(0, 0, 0);
+}
+
+@keyframes shake {
+  10%,
+  90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%,
+  80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%,
+  50%,
+  70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%,
+  60% {
+    transform: translate3d(4px, 0, 0);
+  }
 }
 </style>
