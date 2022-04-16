b2c9cedcb3c8b5d6661671d98cd75718e9024c0f
Julian Schmidt <julian.schmidt@cobalt.io>
2022-04-16 13:04:37 +0200

    feat: show past shows

---
+++ src/components/PastShow.vue
@@ -0,0 +1,28 @@
+<template>
+  <li>{{ displayDate }}<br />{{ show.text }}</li>
+</template>
+
+<script lang="ts">
+import dayjs from "dayjs";
+import { defineComponent, PropType } from "vue";
+
+interface Show {
+  date: string;
+  text: string;
+}
+
+export default defineComponent({
+  name: "PastShow",
+  props: {
+    show: {
+      type: Object as PropType<Show>,
+      required: true,
+    },
+  },
+  computed: {
+    displayDate(): string {
+      return dayjs(this.show?.date).format("MMMM YYYY");
+    },
+  },
+});
+</script>
