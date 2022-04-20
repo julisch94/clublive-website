3808a7740dcb86c76bef252daf501aca4f5f1a34
Julian Schmidt <julian.schmidt@cobalt.io>
2022-04-21 00:10:01 +0200

    feat: update impressum

--- src/views/Impressum.vue
+++ src/views/Impressum.vue
@@ -5,108 +5,53 @@
         <header class="major">
           <h1>Impressum</h1>
         </header>
-
-        <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+        <h2 id="m46">Diensteanbieter</h2>
+        <p>Julian Schmidt</p>
+        <p>Ludwig-Marum-Str. 38</p>
+        <p>76185 Karlsruhe</p>
+        <p>Germany</p>
+        <h2 id="m56">Kontaktmöglichkeiten</h2>
+        E-Mail-Adresse:
+        <p><a href="mailto:contact@clublive.band">contact@clublive.band</a></p>
+        Telefon:
+        <p>01781429505</p>
+        Kontaktformular:
+        <p>siehe unten</p>
+        <h2 id="m172">Social Media und andere Onlinepräsenzen</h2>
+        Dieses Impressum gilt auch für die folgenden Social-Media-Präsenzen und
+        Onlineprofile:
         <p>
-          ClubLive<br />
-          Ludwig-Marum-Stra&szlig;e 38<br />
-          76185 Karlsruhe
-        </p>
-
-        <p>
-          <strong>Vertreten durch die Gesellschafter:</strong><br />
-          Julian Schmidt
-        </p>
-
-        <h2>Kontakt</h2>
-        <p>
-          E-Mail:
-          <a href="mailto:contact@clublive.band">
-            contact@clublive.band
-          </a>
-        </p>
-
-        <p>
-          Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
-          vor einer Verbraucherschlichtungsstelle teilzunehmen.
-        </p>
-
-        <h2>Haftung f&uuml;r Inhalte</h2>
-        <p>
-          Als Diensteanbieter sind wir gem&auml;&szlig; &sect; 7 Abs.1 TMG
-          f&uuml;r eigene Inhalte auf diesen Seiten nach den allgemeinen
-          Gesetzen verantwortlich. Nach &sect;&sect; 8 bis 10 TMG sind wir als
-          Diensteanbieter jedoch nicht verpflichtet, &uuml;bermittelte oder
-          gespeicherte fremde Informationen zu &uuml;berwachen oder nach
-          Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit
-          hinweisen.
+          <a href="https://www.facebook.com/ClubLive.band/" target="_blank"
+            >https://www.facebook.com/ClubLive.band/</a
+          >
         </p>
         <p>
-          Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
-          Informationen nach den allgemeinen Gesetzen bleiben hiervon
-          unber&uuml;hrt. Eine diesbez&uuml;gliche Haftung ist jedoch erst ab
-          dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung
-          m&ouml;glich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen
-          werden wir diese Inhalte umgehend entfernen.
+          <a href="https://instagram.com/clublive.band" target="_blank"
+            >https://instagram.com/clublive.band</a
+          >
         </p>
-        <h3>Haftung f&uuml;r Links</h3>
+        <h2 id="m65">Haftungs- und Schutzrechtshinweise</h2>
         <p>
-          Unser Angebot enth&auml;lt Links zu externen Websites Dritter, auf
-          deren Inhalte wir keinen Einfluss haben. Deshalb k&ouml;nnen wir
-          f&uuml;r diese fremden Inhalte auch keine Gew&auml;hr &uuml;bernehmen.
-          F&uuml;r die Inhalte der verlinkten Seiten ist stets der jeweilige
-          Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten
-          Seiten wurden zum Zeitpunkt der Verlinkung auf m&ouml;gliche
-          Rechtsverst&ouml;&szlig;e &uuml;berpr&uuml;ft. Rechtswidrige Inhalte
-          waren zum Zeitpunkt der Verlinkung nicht erkennbar.
+          Hinweise auf Rechtsverstöße: Sollten Sie innerhalb unseres
+          Internetauftritts Rechtsverstöße bemerken, bitten wir Sie uns auf
+          diese hinzuweisen. Wir werden rechtswidrige Inhalte und Links nach
+          Kenntnisnahme unverzüglich entfernen.
         </p>
-        <p>
-          Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch
-          ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
-          Bekanntwerden von Rechtsverletzungen werden wir derartige Links
-          umgehend entfernen.
-        </p>
-        <h3>Urheberrecht</h3>
-        <p>
-          Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
-          Seiten unterliegen dem deutschen Urheberrecht. Die
-          Vervielf&auml;ltigung, Bearbeitung, Verbreitung und jede Art der
-          Verwertung au&szlig;erhalb der Grenzen des Urheberrechtes
-          bed&uuml;rfen der schriftlichen Zustimmung des jeweiligen Autors bzw.
-          Erstellers. Downloads und Kopien dieser Seite sind nur f&uuml;r den
-          privaten, nicht kommerziellen Gebrauch gestattet.
-        </p>
-        <p>
-          Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt
-          wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden
-          Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf
-          eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
-          entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
-          werden wir derartige Inhalte umgehend entfernen.
-        </p>
-
-        <h2>Dritte</h2>
-        <ul>
-          <li>
-            Website-Betreiber: <a href="https://www.ionos.de/">IONOS</a>,
-            Kontakt: 0721 / 9605727 oder
-            <a href="https://www.1und1.de/Kontakt">Kontaktformular </a>
-          </li>
-          <li>
-            <a href="https://www.google.com/intl/de/contact/"> Google Fonts </a
-            >: Google LLC, 1600 Amphitheatre Parkway, Mountain View, CA 94043
-            USA
-          </li>
-          <li>
-            <a href="https://www.youtube.com/t/contact_us?hl=de&amp;gl=DE"
-              >YouTube</a
-            >, LLC, 901 Cherry Ave., San Bruno, CA 94066, USA
-          </li>
-        </ul>
-
-        <p>
-          Quelle:
-          <a href="https://www.e-recht24.de">https://www.e-recht24.de</a>
+        <h2 id="m169">Bildnachweise</h2>
+        Bildquellen und Urheberrechtshinweise:
+        <p>Roman Bischoff, Timothée Kammies, Yannick Bodemer</p>
+        <p class="seal">
+          <a
+            href="https://datenschutz-generator.de/"
+            title="Rechtstext von Dr. Schwenke - für weitere Informationen bitte anklicken."
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+            ><img
+              src="https://datenschutz-generator.de/wp-content/plugins/ts-dsg/images/dsg-seal/dsg-seal-doc-de.png"
+              alt="Rechtstext von Dr. Schwenke - für weitere Informationen bitte anklicken."
+              width="250"
+              height="250"
+          /></a>
         </p>
       </div>
     </section>
