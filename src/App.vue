fd80f387ce2fd634001ebc677ac4a73b7de8815c
Julian Schmidt <julian.schmidt@cobalt.io>
2022-04-27 13:56:45 +0200

    feat: cookie-consent

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <div id="overlay" :class="{'is-menu-visible': isMenuVisible}">
-
-    <Header @toggle-menu="toggleMenu" :is-transparent="showTransparentNavbar"/>
+  <div id="overlay" :class="{ 'is-menu-visible': isMenuVisible }">
+    <Header @toggle-menu="toggleMenu" :is-transparent="showTransparentNavbar" />
 
     <nav id="menu">
       <div class="inner">
         <ul class="links">
           <li v-for="item in routes" :key="item.name">
-            <router-link @click.capture="toggleMenu()" :to="item.route">{{ item.name }}</router-link>
+            <router-link @click.capture="toggleMenu()" :to="item.route">
+              {{ item.name }}
+            </router-link>
           </li>
         </ul>
       </div>
@@ -15,81 +16,94 @@
     </nav>
 
     <div id="wrapper">
-      <router-view @is-main-in-view="onMainInView($event)"/>
+      <router-view @is-main-in-view="onMainInView($event)" />
 
-      <Contact/>
-      <Footer/>
+      <Contact />
+      <Footer />
     </div>
   </div>
+
+  <CookieConsent />
 </template>
 
 <script lang="ts">
-
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 import Contact from "@/components/Contact.vue";
 import Footer from "@/components/Footer.vue";
 import Header from "@/components/Header.vue";
+import CookieConsent from "@/components/CookieConsent.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     Header,
     Contact,
-    Footer
+    Footer,
+    CookieConsent,
   },
   data() {
     return {
       routes: [
         {
           name: "Home",
-          route: "/"
+          route: "/",
         },
         {
           name: "Musik",
-          route: "/music"
+          route: "/music",
         },
         {
           name: "Band",
-          route: "/band"
+          route: "/band",
         },
         {
           name: "Shows",
-          route: "/shows"
+          route: "/shows",
         },
         {
           name: "Downloads",
-          route: "/downloads"
+          route: "/downloads",
         },
         {
           name: "Datenschutzerklärung",
-          route: "/datenschutzerklaerung"
+          route: "/datenschutzerklaerung",
         },
         {
           name: "Impressum",
-          route: "/impressum"
-        }
+          route: "/impressum",
+        },
       ],
       isMenuVisible: false,
       isMainInView: false,
-    }
+    };
   },
   computed: {
     showTransparentNavbar(): boolean {
       return this.$route.path === "/" && !this.isMainInView;
-    }
+    },
   },
   methods: {
     toggleMenu() {
       this.isMenuVisible = !this.isMenuVisible;
-      console.log('menu visible:', this.isMenuVisible)
     },
     onMainInView(isInView: boolean) {
       this.isMainInView = isInView;
-    }
-  }
+    },
+  },
 });
 </script>
 
 <style>
 @import "./assets/css/main.css";
+
+#cookie-consent {
+  background-color: #242943;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 1em;
+  padding: 1em;
+  position: sticky;
+  bottom: 0;
+}
 </style>
---
+++ src/components/CookieConsent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div v-if="show" class="cookie-consent">
+    <p>
+      Wir verwenden Cookies, um Inhalte und Anzeigen zu personalisieren,
+      Funktionen für soziale Medien anbieten zu können und die Zugriffe auf
+      unsere Website zu analysieren.
+    </p>
+    <button @click="hide()">Okay</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CookieConsent",
+  data() {
+    return {
+      show: false,
+    };
+  },
+  mounted() {
+    this.show = !this.$cookies.get("cookie-consent");
+  },
+  methods: {
+    hide(): void {
+      this.$cookies.set("cookie-consent", 1);
+      this.show = false;
+    },
+  },
+});
+</script>
+
+<style scoped>
+div.cookie-consent {
+  position: sticky;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 12px;
+  gap: 1em;
+  padding: 1em;
+  border-top: solid 1px rgba(212, 212, 255, 0.1);
+  background-color: #2e3450;
+  transform: translateY(100px);
+  animation: moveup 0.2s 1.5s ease-out forwards;
+}
+
+@keyframes moveup {
+  from {
+    transform: translateY(100px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+div.cookie-consent > * {
+  margin: 0;
+}
+</style>
