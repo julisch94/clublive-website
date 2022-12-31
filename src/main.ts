0336871ca4b940db1f9f520e2cb77d44f754c21d
Julian Schmidt <julian.schmidt@cobalt.io>
2022-12-31 15:53:12 +0100

    use library

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueCookies from 'vue-cookies'
+import { createHead } from '@vueuse/head'
 
-const app = createApp(App).use(VueCookies).use(router)
+const app = createApp(App)
+const head = createHead()
+
+app.use(VueCookies).use(router).use(head)
 
 router.isReady().then(() => app.mount('#app'))
