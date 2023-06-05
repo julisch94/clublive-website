4fb6b7a47d4051a10f9df92e00fb71b23a801228
Julian Schmidt <julian.schmidt@cobalt.io>
2023-06-05 15:04:51 +0200

    improve press test and use on home page, too

--- src/data/pressText.ts
+++ src/data/pressText.ts
@@ -1,13 +1,10 @@
 export const pressText = `
-  Wer sich fragt, ob die heutige Radiomusik überhaupt noch
-  live gespielt werden kann, der kommt bei ClubLive ganz 
-  auf seine Kosten. Von Parov Stelar über Clean Bandit, bis 
-  hin zu Robin Schulz und Felix Jaehn werden alle Songs 
-  handgemacht interpretiert. Dabei zeigt ClubLive, dass 
-  nicht nur DJs mit aktuellen Hits für ordentlich Party 
-  sorgen können. 2018 feierte ClubLive Bühnenpremiere und 
-  ist seit dem im Großraum Karlsruhe unterwegs. Vom Club 
-  bis zur Open Air Show geht bei  ClubLive garantiert 
-  keiner ohne verschwitztes Shirt und zerstörte Frisur nach
-  Hause.
+  Wer sich fragt, ob moderne, hoch-produzierte Musik überhaupt noch 
+  live gespielt werden kann, der kommt bei ClubLive ganz auf seine 
+  Kosten. Avicii, David Guetta, Flo Rida, Calvin Harris, Dua Lipa 
+  uvm. werden handgemacht interpretiert. Dabei zeigt ClubLive, dass 
+  nicht nur DJs mit aktuellen Hits für ordentlich Party sorgen 
+  können. Egal ob Club oder Open Air Show – bei ClubLive geht
+  garantiert keiner ohne verschwitztes Shirt und zerstörte Frisur 
+  nach Hause.
 `
