7aa8578a8cd3ba638fdcda92e1e67e3b4b400ac3
Julian Schmidt <julian.schmidt@cobalt.io>
2023-08-04 13:02:28 +0200

    feat: install Playwright, add first tests

---
+++ e2e/home.spec.ts
@@ -0,0 +1,64 @@
+import { test, expect } from '@playwright/test'
+
+test('header bar contains title and menu button', async ({ page }) => {
+  await page.goto('/')
+
+  await expect(page.getByRole('link', { name: 'ClubLive Club-Hits and Charts' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Menu' })).toBeVisible()
+})
+
+test('page title is correct', async ({ page }) => {
+  await page.goto('/')
+
+  expect(await page.title()).toBe('ClubLive | Live Band aus Karlsruhe | Wir spielen Clubmusik live!')
+})
+
+test('banner contains title, subtitle, call to action and social media icons', async ({ page }) => {
+  await page.goto('/')
+
+  await expect(page.getByRole('heading', { name: 'ClubLive' })).toBeVisible()
+  await expect(page.getByText('Live Band aus Karlsruhe.')).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Jetzt reinhören' })).toBeVisible()
+
+  await expect(page.locator('#banner').getByRole('link', { name: 'Instagram' })).toHaveAttribute(
+    'href',
+    'https://instagram.com/clublive.band'
+  )
+  await expect(page.locator('#banner').getByRole('link', { name: 'Facebook' })).toHaveAttribute(
+    'href',
+    'https://facebook.com/ClubLive.band'
+  )
+  await expect(page.locator('#banner').getByRole('link', { name: 'YouTube' })).toHaveAttribute(
+    'href',
+    'https://www.youtube.com/channel/UCpnhZgIseF5XWs5icvU4nuQ'
+  )
+})
+
+test('when user clicks on the call to action button, the browser scrolls #start section into view', async ({
+  page,
+}) => {
+  await page.goto('/')
+
+  await page.getByRole('link', { name: 'Jetzt reinhören' }).click()
+
+  expect(page.url()).toContain('#start')
+})
+
+test('start section contains heading, video and info paragraph', async ({ page }) => {
+  await page.goto('/#start')
+
+  await expect(page.locator('#start').getByRole('heading', { name: 'Wir spielen Clubmusik live!' })).toBeVisible()
+  await expect(page.locator('#start iframe[title="ClubLive YouTube"]')).toBeVisible()
+  await expect(
+    page.locator('#start').getByText('Wer sich fragt, ob moderne, hoch-produzierte Musik überhaupt')
+  ).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Schreibe uns' })).toBeVisible()
+})
+
+test('home link leads back to root', async ({ page }) => {
+  await page.goto('/#start')
+
+  await page.getByRole('link', { name: 'ClubLive Club-Hits and Charts' }).click()
+
+  await page.waitForURL('/')
+})
--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,17 +1,17 @@
 <template>
   <ul class="icons">
     <li>
-      <a href="https://instagram.com/clublive.band" class="icon alt fa-instagram">
+      <a href="https://instagram.com/clublive.band" aria-label="Instagram" class="icon alt fa-instagram">
         <span class="label">Instagram</span>
       </a>
     </li>
     <li>
-      <a href="https://facebook.com/ClubLive.Band" class="icon alt fa-facebook">
+      <a href="https://facebook.com/ClubLive.band" aria-label="Facebook" class="icon alt fa-facebook">
         <span class="label">Facebook</span>
       </a>
     </li>
     <li>
-      <a href="https://www.youtube.com/channel/UCpnhZgIseF5XWs5icvU4nuQ" class="icon alt fa-youtube">
+      <a href="https://www.youtube.com/channel/UCpnhZgIseF5XWs5icvU4nuQ" aria-label="YouTube" class="icon alt fa-youtube">
         <span class="label">YouTube</span>
       </a>
     </li>
