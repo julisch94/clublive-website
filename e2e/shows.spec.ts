16e7c60d10874a28ead2d40967a8d817bb481e09
Julian Schmidt <julian.schmidt@cobalt.io>
2023-08-04 21:57:02 +0200

    test: add e2e tests for shows

--- e2e/shows.spec.ts
+++ e2e/shows.spec.ts
@@ -3,8 +3,36 @@ import { test, expect } from '@playwright/test'
 test('open up menu and go to shows', async ({ page }) => {
   await page.goto('/')
 
-  await page.getByRole('link', { name: 'Menu' }).click();
-  await page.getByRole('link', { name: 'Shows', exact: true }).click();
+  await page.getByRole('link', { name: 'Menu' }).click()
+  await page.getByRole('link', { name: 'Shows', exact: true }).click()
 
   await expect(page.getByRole('heading', { name: 'Shows' })).toBeVisible()
+})
+
+test('shows page is loaded correctly', async ({ page }) => {
+  await page.goto('/shows')
+
+  await expect(page.getByRole('heading', { name: 'Shows' })).toBeVisible()
+  await expect(
+    page.getByText('Schaue hier regelmäßig vorbei um zu sehen, wann wir wieder in deine Nähe kommen.')
+  ).toBeVisible()
+  await expect(page.getByRole('heading', { name: 'Referenzen' })).toBeVisible()
+  await expect(page.getByRole('heading', { name: 'Kontaktformular / Buchung' })).toBeVisible()
+  await expect(page.getByText('Um keinen Auftritt von uns zu verpassen, folge uns bei Instagram')).toBeVisible()
+})
+
+test('references are loaded correctly', async ({ page }) => {
+  await page.goto('/shows')
+
+  await expect(page.getByRole('heading', { name: 'Referenzen' })).toBeVisible()
+
+  // only checking a few
+  await expect(page.locator('[data-test-id="references"]').getByText('Freudenstadt', { exact: true })).toBeVisible()
+  await expect(page.locator('[data-test-id="references"]').getByText('Bad Wildbad', { exact: true })).toBeVisible()
+  await expect(page.locator('[data-test-id="references"]').getByText('Karlsruhe', { exact: true })).toBeVisible()
+  await expect(page.locator('[data-test-id="references"]').getByText('Pforzheim', { exact: true })).toBeVisible()
+  await expect(page.locator('[data-test-id="references"]').getByText('Ettlingen', { exact: true })).toBeVisible()
+  await expect(
+    page.locator('[data-test-id="references"]').getByText('Vaihingen a.d. Enz', { exact: true })
+  ).toBeVisible()
 })
