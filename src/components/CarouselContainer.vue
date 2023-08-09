10872749dfd49969e62f5f031e2bacd7f713493e
Julian Schmidt <julian.schmidt@cobalt.io>
2023-08-09 12:56:33 +0200

    use vueperslides for carousel

--- src/components/CarouselContainer.vue
+++ src/components/CarouselContainer.vue
@@ -1,21 +1,17 @@
 <template>
-  <div class="carousel-container" :style="{ backgroundImage: 'url(' + image + ')' }">
-    <div class="image-overlay"></div>
+  <div class="carousel-container">
+    <vueper-slides fade autoplay fixed-height="100%" :arrows="false">
+      <vueper-slide v-for="(slide, index) in slides" :key="index" :image="slide.image" :duration="8000" />
+    </vueper-slides>
 
-    <div class="image-circle-row">
-      <div
-        v-for="(imageUrl, index) in imageUrls"
-        :key="imageUrl"
-        class="image-circle"
-        :class="{ active: index === activeImageIndex }"
-        @click="changeActiveImage(index)"
-      ></div>
-    </div>
+    <div class="image-overlay"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
 import image1 from '@/assets/images/banner/Club-Live-231.jpg'
 import image2 from '@/assets/images/banner/Club-Live-65.jpg'
 import image3 from '@/assets/images/banner/Club-Live-258.jpg'
@@ -23,44 +19,29 @@ import image4 from '@/assets/images/banner/Club-Live-81.jpg'
 import image5 from '@/assets/images/banner/Club-Live-261.jpg'
 import image6 from '@/assets/images/banner/Club-Live-194.jpg'
 
-const imageUrls = [image1, image2, image3, image4, image5, image6]
-
-const activeImageIndex = ref(0)
-const image = ref(imageUrls[activeImageIndex.value])
-
-const shouldUseCarousel = true
-let intervalId
-
-onMounted(() => {
-  if (shouldUseCarousel) {
-    startImageRotation()
-  }
-})
-
-onUnmounted(() => {
-  clearInterval(intervalId)
-})
-
-const updateBackground = () => {
-  image.value = imageUrls[activeImageIndex.value]
-}
-
-const startImageRotation = () => {
-  intervalId = setInterval(() => {
-    activeImageIndex.value = (activeImageIndex.value + 1) % imageUrls.length
-    updateBackground()
-  }, 5000)
-}
-
-const changeActiveImage = index => {
-  activeImageIndex.value = index
-  updateBackground()
-  clearInterval(intervalId)
-  startImageRotation()
-}
+const slides = [
+  {
+    image: image1,
+  },
+  {
+    image: image2,
+  },
+  {
+    image: image3,
+  },
+  {
+    image: image4,
+  },
+  {
+    image: image5,
+  },
+  {
+    image: image6,
+  },
+]
 </script>
 
-<style scoped>
+<style>
 .carousel-container {
   position: absolute;
   top: 0;
@@ -80,34 +61,21 @@ const changeActiveImage = index => {
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5); /* Adjust the opacity as needed */
+  z-index: 1;
 }
 
-.image-circle-row {
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  bottom: 1em;
-  left: 0;
-  right: 0;
+button.vueperslides__bullet {
+  border: none;
+  box-shadow: none;
 }
 
-@media screen and (max-width: 760px) {
-  .image-circle-row {
-    display: none;
-  }
+button.vueperslides__bullet,
+button.vueperslides__bullet:hover {
+  border: none;
+  box-shadow: none;
 }
 
-.image-circle {
-  width: 10px;
-  height: 10px;
-  margin: 0 0.5rem;
-  border-radius: 50%;
-  background-color: rgba(255, 255, 255, 0.3);
-  cursor: pointer;
-}
-
-.image-circle.active {
-  background-color: white;
+button.vueperslides__bullet--active {
+  color: #9bf1ff;
 }
 </style>
