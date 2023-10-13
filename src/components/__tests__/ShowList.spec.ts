2ac1627bd1cacf97530747ca8d402508c16c939f
Julian Schmidt <contact@julianschmidt.codes>
2023-10-13 09:20:24 +0200

    add unit tests (#323)
    * add userEvent library
    
    * externalize api call
    
    * add unit tests for contact form
    
    * mock scrollIntoView
    
    * rewrite ContactForm in composition api
    
    * use returned screen

--- src/components/__tests__/ShowList.spec.ts
+++ src/components/__tests__/ShowList.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect, beforeEach, afterEach } from 'vitest'
 import ShowList from '../ShowList.vue'
-import { render, screen } from '@testing-library/vue'
+import { render } from '@testing-library/vue'
 import { vi } from 'vitest'
 import { ShowModel } from '@/model/show.model'
 
@@ -40,11 +40,13 @@ test('future shows are displayed', () => {
     return { shows }
   })
 
-  render(ShowList)
+  const screen = render(ShowList)
 
   expect(screen.queryAllByTestId('show').length).toBe(3)
   expect(screen.getByText(/The Irish Pub, Pforzheim/)).to.exist
   expect(screen.getByText(/Private Veranstaltung, Karlsruhe/)).to.exist
   expect(screen.getByText(/Gig this evening/)).to.exist
   expect(screen.queryByText(/Old gig/i)).not.to.exist
+
+  screen.unmount()
 })
---
+++ src/utils/api.ts
@@ -0,0 +1,20 @@
+export const sendContactForm = async (values: {
+  name: string
+  email: string
+  message: string
+  reference: string
+  referenceOther: string
+}) => {
+  const body = {
+    'form-name': 'clublive-contact',
+    ...values,
+  }
+
+  return fetch('/api/contact', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: new URLSearchParams(body).toString(),
+  })
+}
